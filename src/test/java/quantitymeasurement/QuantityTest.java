584296042a2ee5ffa30444c62dfc3d50be7e066d
Ruchir Dixit <ruchirtd96@gmail.com>
2020-10-13 15:51:09 +0530

    [Ruchir]Added: Test case to check 0 inch equal to 0 inch and 0 inch and 1 inch

--- src/test/java/quantitymeasurement/QuantityTest.java
+++ src/test/java/quantitymeasurement/QuantityTest.java
@@ -6,10 +6,33 @@ import org.junit.Test;
 public class QuantityTest {
 
     @Test
-    public void given0Feetand0Feet_ShouldReturnEqual()
+    public void given0FeetAnd0Feet_ShouldReturnEqual()
     {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1,feet2);
+    }
+
+    @Test
+    public void given0FeetAnd1Feet_ShouldReturnNotEqual()
+    {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1,feet2);
+    }
+
+    @Test
+    public void given0InchAnd0Inch_ShouldReturnEqual()
+    {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0.0);
+        Assert.assertEquals(inch1,inch2);
+    }
+    @Test
+    public void given0InchAnd1Inch_ShouldReturnNotEqual()
+    {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        Assert.assertNotEquals(inch1,inch2);
     }
 }
